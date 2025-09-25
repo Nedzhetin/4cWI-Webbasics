57fb7a5f81e180763e7570b714be8c372f18fc38
Nejo <nedzhetin.nedzhetin@student.htldornbirn.at>
2025-09-25 14:01:13 +0200

    TS_basics_done

--- Webbasics/TS/demo.ts
+++ Webbasics/TS/demo.ts
@@ -1,10 +1,34 @@
 let a:number = 10;
 let b:number = 20;
-let Name: string = "John Doe";
+let myName: string = "John Doe";
+let names: string[] = ["Alice", "Bob", "Charlie"];
+let isActive: boolean = true;
+let data: object = { id: 1, value: "Sample" };
+
+interface Person{
+    name: string;
+    age: number;
+    isEmployed: boolean;
+}
+
+let person: Person = {
+    name: "Luca Hagspiel",
+    age: 18,
+    isEmployed: true
+};
+
+console.log(person.name);
+
 
 function add(x:number, y:number):number {
     return x + y;
 }
+
+function greet(name:string):string{
+    return "Hello, " + name + "!";
+}
+
+console.log(greet(myName));
 
 
 let result = add(a, b);
