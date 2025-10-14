06d21d51d323fa74a75453cc2bf209c76e557105
Nejo <nedzhetin.nedzhetin@student.htldornbirn.at>
2025-10-14 11:47:33 +0200

    react_learing_Zustand

---
+++ Webbasics/React/react-zustand/src/Counter.tsx
@@ -0,0 +1,27 @@
+import { useCounterStore } from "./store.ts";
+import { useEffect } from "react";
+
+const setCount = () => {
+  useCounterStore.setState({ count: 10 });
+};
+
+function Counter({ count }: { count: number }) {
+  const incrementAsync = useCounterStore((state) => state.incrementAsync);
+  const decrement = useCounterStore((state) => state.decrement);
+
+  useEffect(() => {
+    setCount();
+  }, []);
+
+  return (
+    <>
+      <div className="text-3xl">{count}</div>
+      <button className="m-10" onClick={incrementAsync}>
+        increase Async
+      </button>
+      <button onClick={decrement}>decrease</button>
+    </>
+  );
+}
+
+export default Counter;
