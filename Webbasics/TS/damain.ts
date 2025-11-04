16d3a0906eadde3779184deaa021f4c4f95f9ad2
Nejo <nedzhetin.nedzhetin@student.htldornbirn.at>
2025-11-04 10:08:16 +0100

    testing

--- Webbasics/TS/damain.ts
+++ Webbasics/TS/damain.ts
@@ -2,3 +2,8 @@ let a: number = 10;
 let b: string = "Hello";
 let c: boolean = true;
 let d: object = { name: "John", age: 30 };
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
